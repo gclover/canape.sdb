0e99a0f3edabd04ddd3ddbff9de3c355389ec47e
gclover <clover.gch@gmail.com>
2013-03-15 02:26:41 +0800

    add cluster sdb client

--- canape/sdb/conn.py
+++ canape/sdb/conn.py
@@ -29,9 +29,24 @@ class Sdb(object):
 		except Exception, e:
 			print e
 		
-
+class ClusterSdb(object):
 	
+	import ring_;
 
+	def __init__(self, nodes):
+		self.ring = ring_.Ring();
+		self.nodes = {}
+		for (name, addr) in enumerate(nodes):
+			self.nodes[name] = Sdb(addr)
+			self.ring.addnode(name)
 
+	def close(self):
+		for sdb in self.nodes.values():
+			sdb.close()
+	
+	def put(self, k, v)
+		return self.nodes[self.ring.getnode(k)].put(k, v)
 
+	def get(self, k)
+		return self.nodes[self.ring.getnode(k)].get(k)
 
